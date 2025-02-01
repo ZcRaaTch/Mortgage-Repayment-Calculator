6e845f08ab0a0143df6d1d6175beab079f3c38e0
Abhinav Awana <abhinavawana2444@gmail.com>
2025-02-02 01:25:28 +0530

    color pallet setting and implementatin testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
+import illustrationImg from "./assets/images/illustration-empty.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1 className="text-6xl text-yellow-800">hello</h1>
+      <div className="flex gap-0 bg-slate-100 h-screen justify-center items-center">
+        <div className="flex bg-white">
+          <div id="repayment-section ">
+            <h1 className="font-sans font-bold">Mortgage Calculator</h1>
+          </div>
+          <div id="results-section">
+            <img src={illustrationImg} />
+          </div>
+        </div>
       </div>
     </>
   );
